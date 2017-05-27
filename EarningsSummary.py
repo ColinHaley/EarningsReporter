60b1bda0f7458f16c3e25e413b3e833da38632b7
Colin Haley <admin@colinhaley.net>
2017-05-26 22:17:28 -0400

    Add base urls with no context

--- EarningsSummary.py
+++ EarningsSummary.py
@@ -1,6 +1,13 @@
 __author__ = "Colin Haley"
 
+import logging, sys
 from BeautifulSoup import BeautifulSoup
+import requests
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+today_earnings_whisper_url = "https://www.earningswhispers.com/calendar?sb=p&d=0&t=all"
 
 if __name__ == "__main__":
-    # do stuff
+    r = requests.get(today_earnings_whisper_url)
+
+
