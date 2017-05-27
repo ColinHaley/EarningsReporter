edf4b908d1865b9fcfc91f540ed71e1dc98f9f65
Colin Haley <admin@colinhaley.net>
2017-05-26 23:40:23 -0400

    Apparently individual li tags under parent ul have a shared  class

--- EarningsSummary.py
+++ EarningsSummary.py
@@ -3,15 +3,19 @@ __author__ = "Colin Haley"
 import logging, sys
 from BeautifulSoup import BeautifulSoup
 import requests
+import jinja2
 
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 today_earnings_whisper_url = "https://www.earningswhispers.com/calendar?sb=p&d=0&t=all"
 
+def send_email(content):
+    #send it home folks
+
+
 if __name__ == "__main__":
     r = requests.get(today_earnings_whisper_url).content
     soup = BeautifulSoup(r)
-    earnings_data = soup.findAll("ul", {"id":"epscalendar"})
-
+    earnings_data = soup.findAll("li", {"class":"cor bmo showconf nwh"})
 
 
 
