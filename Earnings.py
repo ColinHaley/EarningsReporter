04e365666a4c7407e79271773047bcfe0a5e3100
Colin Haley <admin@colinhaley.net>
2017-05-27 12:20:03 -0400

    Remove some scope creep

--- Earnings.py
+++ Earnings.py
@@ -1,45 +1,35 @@
 class Earnings(object):
     """An individual event parsed from https://www.earningswhispers.com/calendar?sb=p&d=1&t=all&v=t
     
-    d = 0 is today. ++ or -- for prior and current days
+    d = 0 is today. d++ for days
 
-    Attributes [prefix of # indicates typically only populated for prior days]:
-        Symbol: A string representing Ticker Symbol
-        Company: A string representing Company Name
+    Attributes:
         Confirmed: A Boolean representing confirmation of an earnings report.
-        #Last Confirmed: A string representing the time of the earnings report taking place.
+        Company: A string representing Company Name
+        Symbol: A string representing Ticker Symbol
+        Last Confirmed: A string representing the time of the earnings report taking place.
         Estimated EPS: 
         Estimated Revenue:
-        #Reported EPS:
-        #Reported Revenue:
-        #ESP Surprise %:
-        #Revenue Surprise %:
-        #Earnings Growth
-        #Revenue Growth
-
     """
+
+    def __init__(self, earnings_html):
+        """parse html objects to variables
+        
+        This is absolutely going to need some optimization. findAll calls have lots of overhead.
+        """
+        self.confirmed = earnings_html.findAll('div', {'class':'confirm icon-check'})
+        self.company = earnings_html.findAll('div', {'class':'company'})
+        self.ticker = earnings_html.findAll('div', {'class':'ticker'})
+        self.time = earnings_html.findAll('div', {'class':'time'}) # earnings datetime
+        self.estimated_eps = earnings_html.findAll('div', {'class':'estimate'}) # eps estimate
+        self.estimated_revenue = earnings_html.findAll('div', {'class':'revestimate'}) #evenue estimate
+
+    #def lookup_logo(self):
+        # lookup logo using 
 
     __logo_base__ = "http://cdn.instantlogosearch.com/png?id=instantlogosearch-{0}"
+        
 
-def parse_html(soup_object):
-    """parse html objects to variables
-    
-    This is absolutely going to need some optimization. 
-    """
 
-    soup_object.findAll('div',{'class':'confirm icon-check'})
-    soup_object.findAll('div',{'class':'company'})
-    soup_object.findAll('div',{'class':'ticker'})
-    soup_object.findAll('div',{'class':'actestimate'}) #reported eps
-    soup_object.findAll('div',{'class':'time'} # earnings datetime
-    soup_object.findAll('div',{'class':'estimate'}) # eps estimate
-    soup_object.findAll('div',{'class':'revestimate'}) #evenue estimate
-    
-
-def __init__(self, earnings_html):
-    parse_html(earnings_html)
-
-#def lookup_logo(self):
-    # lookup logo using 
 
 
