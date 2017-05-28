0275055cc1b781e08f1eaf41842619889a61d5d4
Colin Haley <admin@colinhaley.net>
2017-05-27 20:53:46 -0400

    Add absolutely basic unformed try|except logic for empty fields

--- Earnings.py
+++ Earnings.py
@@ -4,7 +4,7 @@ class Earnings(object):
     """An individual event parsed from https://www.earningswhispers.com/calendar?sb=p&d=1&t=all&v=t
     d = 0 is today. d++ for days
     Attributes:
-        Confirmed: A Boolean representing confirmation of an earnings report.
+        Confirmed: A string representing confirmation of an earnings report.
         Company: A string representing Company Name
         Symbol: A string representing Ticker Symbol
         Last Confirmed: A string representing the time of the earnings report taking place.
@@ -21,12 +21,30 @@ class Earnings(object):
         This is absolutely going to need some optimization. findAll calls have lots of overhead.
         """
         self.id = uuid.uuid1()
-        self.confirmed = earnings_html.findAll('div', {'class':'confirm icon-check'})[0].text
-        self.company = earnings_html.findAll('div', {'class':'company'})[0].text
-        self.ticker = earnings_html.findAll('div', {'class':'ticker'})
-        self.time = earnings_html.findAll('div', {'class':'time'}) # earnings datetime
-        self.estimated_eps = earnings_html.findAll('div', {'class':'estimate'}) # eps estimate
-        self.estimated_revenue = earnings_html.findAll('div', {'class':'revestimate'}) #evenue estimate
+        try:
+            self.confirmed = earnings_html.findAll('div', {'class':'confirm icon-check'})[0].text
+        except:
+            self.confirmed = "Unconfirmed"
+        try:
+            self.company = earnings_html.findAll('div', {'class':'company'})[0].text
+        except:
+            self.company = "Unspecified"
+        try:
+            self.ticker = earnings_html.findAll('div', {'class':'ticker'})
+        except:
+            self.ticker = "Unspecified"
+        try:
+            self.time = earnings_html.findAll('div', {'class':'time'}) # earnings datetime
+        except:
+            self.time = "Unspecified"
+        try:
+            self.estimated_eps = earnings_html.findAll('div', {'class':'estimate'}) # eps estimate
+        except:
+            self.estimated_eps = "Unspecified"
+        try:
+            self.estimated_revenue = earnings_html.findAll('div', {'class':'revestimate'}) #evenue estimate
+        except:
+            self.estimated_revenue = "Unspecified"
 #    __logo_base__ = "http://cdn.instantlogosearch.com/png?id=instantlogosearch-{0}"
 
     def gaga(self):
