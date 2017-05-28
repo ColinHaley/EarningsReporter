c2522def9ebf90d6aae0ad1a24366d6dbfd27e95
Colin Haley <admin@colinhaley.net>
2017-05-27 23:16:23 -0400

    Show More js function needs to be called for full listing. Will revisit after prototype

--- EarningsSummary.py
+++ EarningsSummary.py
@@ -2,7 +2,7 @@ __author__ = "Colin Haley"
 
 import logging, sys
 from BeautifulSoup import BeautifulSoup
-import requests
+import requests, selenium
 import jinja2
 from Earnings import Earnings
 
@@ -10,30 +10,31 @@ logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 today_earnings_whisper_url = "https://www.earningswhispers.com/calendar?sb=p&d=7&t=all"
 
 #todo: Query <li> under <ul class="epscalendar", ensure len(<li>) == len(earnings_classes)
-earnings_classes = ["cor amc showconf nwh",
-                    "cor amc shownotconf nwh",
-                    "cor amc shownotconf nwh",
-                    "cor bmo showconf nwh",
-                    "cor bmo shownotconf nwh",
-                    "cor bmo shownotconf nwh",
-                    "cor dmh shownotconf nwh",
-                    "cors amc showconf nwh",
-                    "cors amc shownotconf nwh",
-                    "cors bmo shownotconf nwh",
-                    "cors dmh shownotconf nwh"]
 
 if __name__ == '__main__':
-r = requests.get(today_earnings_whisper_url).content
-soup = BeautifulSoup(r)
-earnings_data = []
+    r = requests.get(today_earnings_whisper_url).content
+    soup = BeautifulSoup(r)
+    earnings_data = []
 
-for subclass in earnings_classes:
-    for item in soup.findAll("li", {"class": subclass}):
-        test = Earnings(item)
-        earnings_data.append(test)
+    datasets = []
 
-for thing in earnings_data:
-    thing.gaga()
+    # eps calendar should always exist
+    datasets.append[soup.findAll("ul", {"id":"epscalendar"})[0]]
+
+    lis=epscalendar[0].findAll("li")
+    for li in lis:
+        try:
+            li.findAll("div",{"class":"ticker"})[0].text
+        except:
+            pass
+
+    for subclass in earnings_classes:
+        for item in soup.findAll("li", {"class": subclass}):
+            test = Earnings(item)
+            earnings_data.append(test)
+
+    for thing in earnings_data:
+        thing.gaga()
 
 
 def send_email(content):
